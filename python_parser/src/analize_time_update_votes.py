2b38b528ad38e7b85e36d08782edfcb0ccb3d948
toshiki-h <hirao.toshiki.ho7@is.naist.jp>
2015-12-29 21:39:02 +0900

    updated results

--- python_parser/src/analize_time_update_votes.py
+++ python_parser/src/analize_time_update_votes.py
@@ -19,7 +19,7 @@ from Utils import VoterFunctions
 ### main
 FMT = '%Y-%m-%d %H:%M:%S'
 index = 0
-print "Id,Status,Claster,AverageCorrectness,TotalDays,NumOfUpdated,NumOfVotes"
+print "Id,Status,Claster,AverageCorrectness,ReviewLen,ReviewLenPerVotes,NumOfVotes,NumOfComments"
 for line in open(sys.argv[1], "r"):
     if index == 0:
         index = index + 1
@@ -62,16 +62,23 @@ for line in open(sys.argv[1], "r"):
     csr3.execute("select ReviewId, Message, AuthorId, WrittenOn from Comment where ReviewId = '"+Id+"' order by WrittenOn asc;")
     results3 = csr3.fetchall()
     NumOfVotes = 0
+    NumOfComments = 0
     for result in results3:
         message = result[1].replace("\n", "<br>")
         authorId = result[2]
         if ReviewerFunctions.IsDecisionMaking(message) != 0:
             break
-        if ReviewerFunctions.IsBotTest(authorId) == False and ReviewerFunctions.IsVote(message) != 0:
-            NumOfVotes = NumOfVotes + 1
+        if ReviewerFunctions.IsBotTest(authorId) == False:
+            if ReviewerFunctions.IsVote(message) != 0:
+                NumOfVotes = NumOfVotes + 1
+            else:
+                NumOfComments = NumOfComments + 1
             #print message
 
     #print Id,averageCorrectness,total,days,NumOfUpdated,NumOfVotes
-    print str(Id)+","+str(status)+","+str(claster)+","+str(averageCorrectness)+","+str(diff.total_seconds()/60/60/24)+","+str(updated)+","+str(NumOfVotes)
+    if NumOfVotes != 0:
+        print str(Id)+","+str(status)+","+str(claster)+","+str(averageCorrectness)+","+str(diff.total_seconds()/60/60/24)+","+str((diff.total_seconds()/60/60/24)/NumOfVotes)+","+str(NumOfVotes)+","+str(NumOfComments)
+    else:
+        print str(Id)+","+str(status)+","+str(claster)+","+str(averageCorrectness)+","+str(diff.total_seconds()/60/60/24)+","+str(0)+","+str(NumOfVotes)+","+str(NumOfComments)
 
     #print str(lastWrittenOn)+"****"+str(createdOn)+"****"+str(updated)
